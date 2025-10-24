f4fd7a62cfef3c78e76d7bba7b8d0260dff9f241
ElChavoDelBronx <20243ds087@utez.edu.mx>
2025-10-24 09:46:26 -0600

    V1

---
+++ app.js
@@ -0,0 +1,38 @@
+const inputNameCompany = document.getElementById('inputNameCompany');
+const inputNameLink = document.getElementById('inputNameLink');
+const inputPhone = document.getElementById('inputPhone');
+
+const btnSave = document.getElementById('btnSave');
+
+btnSave.addEventListener('click', ()=>{
+    const nameCompany = inputNameCompany.value;
+    const nameLink = inputNameLink.value;
+    const phone = inputPhone.value;
+    const company = {
+        nameCompany : nameCompany,
+        nameLink,
+        phone
+    }
+    console.log('Objeto')
+    console.log(company);
+    const jsonString = JSON.stringify(company);
+    console.log('String')
+    console.log(jsonString);
+    sessionStorage.setItem('company', jsonString);
+});
+
+document.addEventListener('DOMContentLoaded', ()=>{
+    const dataLocal = sessionStorage.getItem('company');
+
+    if(dataLocal != null){
+        console.log('Hay datos en SessionStorage');
+        console.log(dataLocal);
+        const jsonParse = JSON.parse(dataLocal);
+        console.log('Datos Parseados');
+        console.log(company);
+        inputNameCompany.value = company.nameCompany;
+        inputNameLink.value = company.nameLink;
+        inputPhone.value = company.phone;
+
+    }
+});
